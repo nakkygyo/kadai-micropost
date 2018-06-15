eedfc34a5539a53f7de92fd03f7b6ce48e1a03c1
nakkygyo <natsukigyotoku.yahoo.co.jp>
2018-06-15 06:02:35 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Welcome to Microposts</h1>
+            {!! link_to_route("signup.get", "Sign un now!", null, ["class"=>"btn btn-lg btn-primary"]) !!}
         </div>
     </div>
 @endsection
