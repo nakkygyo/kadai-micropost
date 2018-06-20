84783d260519821cf213483f26f92a69f9e53c93
nakkygyo <natsukigyotoku.yahoo.co.jp>
2018-06-20 07:30:10 +0000

    post

--- resources/views/users/users.blade.php
+++ resources/views/users/users.blade.php
@@ -3,13 +3,16 @@
         @foreach($users as $user)
             <li class="media">
                 <div class="media-left">
-                    <img class="media-object img rounded" src="{{ Gravatar::src($user->email, 50) }}" alt="">
+                    <img class="media-object img-rounded" src="{{ Gravatar::src($user->email, 50) }}" alt="">
                 </div>
                 <div class="media-body">
-                    {{ $user->name }}
-                </div>
-                <div>
-                    <p>{!! link_to_route("users.show", "View.profile", ["id"=>$user->id]) !!}</p>                    
+                    <div>
+                        {{ $user->name }}
+                    </div>
+                
+                    <div>
+                        <p>{!! link_to_route("users.show", "View profile", ["id"=>$user->id]) !!}</p>                    
+                    </div>
                 </div>
             </li>
         @endforeach
--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,8 @@ Route::post("login", "Auth\LoginController@login")->name("login.post");
 Route::get("logout", "Auth\LoginController@logout")->name("logout.get");
 
 Route::group(["middleware"=>["auth"]], function() {
-    Route::resource("users", "UsersController", ["only"=>["index", "show"]]);    
+    Route::resource("users", "UsersController", ["only"=>["index", "show"]]);
+    Route::resource("microposts", "MicropostsController", ["only"=>["store", "destroy"]]);
 });
+
+Route::get("/", "MicropostsController@index");
